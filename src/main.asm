1511e998e2fa7c604cff72af8e21d1161d54a813
FloofyPlasma <github@floofyplasma.com>
2024-12-24 18:24:51 -0500

    Display basic hello world.

---
+++ src/main.asm
@@ -0,0 +1,38 @@
+org 0x7C00
+bits 16
+
+start:
+    jmp main
+
+;
+;   Prints a string to the screen using BIOS interrupts
+;
+print_string:
+    lodsb ; Load byte from SI into AL
+    or al, al ; Check if its a null character
+    jz .print_done
+
+    mov ah, 0xE ; Display char code
+    int 0x10
+
+    jmp print_string ; Since its not a null character, go back to start
+
+.print_done:
+
+    mov ah, 0x3 ; Reset cursor
+    mov al, 0
+    int 0x10
+
+    ret
+
+main:
+    mov si, message_hello
+    call print_string
+    hlt
+
+; 0D 0A (Carrige Return) (Line Feed)
+
+message_hello: db 'Hello, world!', 0x0D, 0x0A, 'This is a new line that hopefully works', 0x0D, 0x0A, 'Heres another line', 0x0D, 0x0A, 'And finally heres a super duper ultra mega long line', 0x0D, 0x0A, 0 ; Null termination
+
+times 510 - ($ - $$) db 0
+dw 0xAA55
